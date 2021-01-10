cfd4798b6ee05e4835b00d2d80846fc3e741e42f
Vidhixa Joshi <Vidhixa@users.noreply.github.com>
2021-01-10 12:19:56 -0800

    Update minimumSwapsToSort.py

--- HackerRank/minimumSwapsToSort.py
+++ HackerRank/minimumSwapsToSort.py
@@ -1,28 +1,42 @@
+#!/bin/python
 
-# Complete the minimumSwaps function below.
+import math
+import os
+import random
+import re
+import sys
+import copy
+
+# Referenced this solution from https://www.hackerrank.com/challenges/minimum-swaps-2/forum/comments/473577
 def minimumSwaps(arr):
+    # Use O(n) space to store the sorted array to be used as reference
+    ref_arr = sorted(arr)
+    # 4 1 2 3
+    index_dict = {v: i for i,v in enumerate(arr)}
+    #{4:0, 1:1, 2:2, 3:3}
     swaps = 0
-    i = 0
-    count = 0
-    
-    while i < len(arr):
-        maxDiff = -1
-        for j in range(i+1, len(arr)):
-            #fptr.write(str(arr) + ", compare " + str(arr[i]) + " and " + str(arr[j]) + '\n')
-            diff = arr[i] - arr[j]
-            if diff >= maxDiff:
-                 maxDiff = diff
-                 maxDiffIndex = j 
-        if maxDiff < 0:
-            i = i+1
-            #fptr.write(str("New i ") + str(i) + "value " + str(arr[i]) + '\n')
+
+    for i,v in enumerate(arr): # 0,4
+        correct_value = ref_arr[i] # 1
+        if v != correct_value: # 4 != 1
+            to_swap_ix = index_dict[correct_value] # to: 1
+            arr[to_swap_ix],arr[i] = arr[i], arr[to_swap_ix] # 1, 4, 2, 3 | 1, 2, 4, 3 | 1, 2, 3, 4
+            index_dict[v] = to_swap_ix
+            index_dict[correct_value] = i
+            swaps += 1 
+            
+    return swaps
         
-        else:
-            temp = arr[i]
-            arr[i] = arr[maxDiffIndex]
-            arr[maxDiffIndex] = temp
-            swaps = swaps + 1
-    #fptr.close()
-        
-    return swaps;
- 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(raw_input())
+
+    arr = map(int, raw_input().rstrip().split())
+
+    res = minimumSwaps(arr)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
